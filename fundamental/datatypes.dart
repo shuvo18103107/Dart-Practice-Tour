71f5d002dbd5735dc1dea3fc1a605caf99caa6a0
shuvo18103107 <18103107@iubat.edu>
2020-11-02 13:36:51 +0600

    data types

---
+++ fundamental/datatypes.dart
@@ -0,0 +1,28 @@
+/*
+int 
+double
+bool
+String
+dynamic
+
+*/
+
+main() {
+  int amount = 100;
+  var amount1 = 200;
+  print('amount 1 : $amount | amount 2 : $amount1');
+
+  double damount = 1054.6398;
+  var damunt1 = 154986.3659;
+  print('damount 1 : $damount | damount 2 : $damunt1');
+  String name = 'Mohammad Ali Shuvo';
+  var name2 = 'Allahu Akbar';
+  print('name1: $name | name 2 : $name2');
+  bool isittrue = true;
+  var isfalse = false;
+  print(' istrue : $isittrue | isfalse : $isfalse');
+  dynamic weakvariable = 100;
+  print('Weakvariabkle 1 : $weakvariable');
+  weakvariable = 'Mohammad Ali Shuvo';
+  print('weakvariable 2 : $weakvariable');
+}
