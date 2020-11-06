f1f1cd1bebb2d0912b233ea58d4e0d66c31e7a42
shuvo18103107 <18103107@iubat.edu>
2020-11-06 20:57:12 +0600

    final vs const vs static const

--- fundamental/class.dart
+++ fundamental/class.dart
@@ -1,4 +1,13 @@
 //class
+//final
+//static
+//static const
+/*
+final vs const
+in final we can change the value in runtime but not in compile time
+and in const we cannot change the value in both run and compile time
+outside the class final and const behave same 
+*/
 class person {
   String name;
   int age;
@@ -27,13 +36,30 @@ class person {
   }
 }
 
+class p {
+  final name; //object property and only initialize by constructor not by object setter
+  static const int age =
+      30; // class property// we cannot initialize this through constructor and object setter
+  p(this.name);
+}
+
 void main() {
   person obj1 = person('Mohammad Ali', 'Shymoli ka banda');
   var obj2 = person('kamrul pa ji', 'Airport ka banda', 21);
   person obj3 = person('Shuvo', 'Shymoli', 25);
   person guestobj = person.guest();
+  var object = p('Kamrul shetji ka jay ho');
+  var object1 = p('john doe');
+
+  // object.name = 'Kamrul vai';// if we use var instead of final then its work
+  // object.name = 'Kamrul vai ka jay hey';
+  // but if we use final keyword in class p in name variable we cannot change the name variable value ;
   obj1.showOutput();
   obj2.showOutput();
   obj3.showOutput();
   guestobj.showOutput();
+  print(object.name);
+  print(object1.name);
+  print(p
+      .age); // class property thats why we cannot use the variable using object we have to access it through classname
 }
