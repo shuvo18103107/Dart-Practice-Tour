c5159cd0ba62c610622fa02c25c24a5683b5c5e3
shuvo18103107 <18103107@iubat.edu>
2020-11-08 17:39:25 +0600

    ⚡️ callable classs in dart

---
+++ fundamental/callableclass.dart
@@ -0,0 +1,25 @@
+//*Callable class
+
+/* 
+*when dart class is called like a function
+*implement call() function
+
+
+*/
+
+void main() {
+  person obj = person();
+  var msg = obj(25, 'kamrulvai');
+
+  print(msg);
+}
+
+// class person {
+//   call(int a, String b) {
+//     print("the name of the guy is : $b and age is : $a");
+//   }
+// }
+class person {
+  String call(int a, String b) =>
+      "the name of the person is : $b and the age is : $a";
+}
