a5efe0975464f8b669b928bb1372c9118cb885b2
shuvo18103107 <18103107@iubat.edu>
2020-11-08 18:37:37 +0600

    ✨ dart collection

---
+++ dart collection/collections.dart
@@ -0,0 +1,78 @@
+//collection
+/*
+3 build in collection in dart
+*list
+*set
+*Map
+
+*/
+void main() {
+  List names = [
+    'kamrul',
+    'Ali',
+    'shuvo',
+    'noushad',
+    125.365,
+    526,
+    5555555555555555555555.2986
+  ];
+  //this list contain only the string value
+  // List<String> name2 = ['john', 'jack', 'jil'];
+  ///List<String> name2 = const ['john', 'jack', 'jil']; cannot change any index now beacuse of const
+  // name2[1] = 'Mark';
+  names[1] = 'Mohammad Ali';
+
+  //var name2 = names;
+  //name2[0] = 'noushad';
+  //clone the list using spread operator
+  var name2 = [...names];
+  // now the change in index will not occure beacuse of spread operator it clone the list we cannot change its position value now
+  names[1] = 'Mohammad Ali shuvo';
+  // remove value from list
+  names.remove('Ali');
+  // Add value to list
+  names.add(24);
+  // remove list value through index
+  names.removeAt(2);
+
+  print(names[0]);
+  print(names.length);
+  for (var n in names) {
+    print(n);
+  }
+  for (var n in name2) {
+    print(n);
+  }
+// set
+/*
+set is a unorder collection of unique items
+*/
+  var halogens = {'florine', 'clorine', 'florine1'};
+  Set<String> hello = {};
+  // hello = <String>{};//it is a linkedhashmap in runtimetype
+  for (var n in halogens) {
+    print(n);
+  }
+  print(hello.runtimeType);
+
+//Map
+/*collection of key value pair item
+*/
+
+  var gifts = {
+    //keys    :  value
+    'first': 'watch',
+    'second': 'flower',
+    'third': 'book',
+    4: 'pen',
+    5: 'Mobile'
+  };
+  gifts['second'] = 'rose';
+  var fruits = Map();
+  fruits['first'] = 'Mango';
+  fruits[2] = 'Orange';
+  print(fruits[2]);
+
+  print(gifts[4]);
+  print(gifts['second']);
+}
